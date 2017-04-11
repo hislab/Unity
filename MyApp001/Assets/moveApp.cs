71616e2b2368bcd7922b9ee9692ec00f54477b85
Takaaki Hishida <hishida@aitech.ac.jp>
2017-04-11 20:17:03 +0900

    First upload
    Unity Sample for Project Practice.

---
+++ MyApp001/Assets/moveApp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveApp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void MoveApp1() {
+		Application.LoadLevel ("app1");
+	}
+	public void MoveApp2() {
+		Application.LoadLevel ("app2");
+	}
+	public void MoveApp3() {
+		Application.LoadLevel ("app3");
+	}
+	public void SetSValue() {
+		
+	}
+}
